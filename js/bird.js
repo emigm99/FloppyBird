344b317374e1f2da170c6f07c5911dbf6ade66f9
ElBarbero <emilio.paterna@gmail.com>
2018-12-26 21:54:23 +0100

    Introducción de los tubos y sus temaños random con la separacion

--- js/bird.js
+++ js/bird.js
@@ -2,19 +2,22 @@
    var posY = 130;
    var posX = 130;
    var teclaC = 0;
+   var velocidad = 2;
 
 
    function iniciar() {
-
+     var tubou = new Image()
+     tubou.src = "./imgs/tubou.png"
 
      fondo();
      partesJuego();
+
      console.log("Iniciado");
    }
 
    function partesJuego() {
-
      setInterval(bird, 80);
+      tubo1();
    }
 
    function fondo() {
@@ -25,23 +28,45 @@
 
    function bird() {
 
-     console.log(posY);
      ctx = lienzo.getContext("2d");
-     ctx.clearRect(0, 0, 300, 600);
-     ctx.beginPath();
-     ctx.fillRect(posX,posY, 50, 50);
+     ctx.clearRect(30, 30, posX, posY);
      ctx.fillStyle = "#F7FE2E";
-     ctx.fill();
-     ctx.closePath();
+     ctx.fillRect(posX,posY, 30, 30);
+
+
 
      if(teclaC != 38){
-       posY = posY + 10;
+       posY = posY + velocidad;
+       velocidad++;
      }
 
      if(posY < 0){
        posY = 0;
      }
+     if(posY > 550){
+       posY = lienzo.height-30;
+     }
 
+   }
+
+   function tubo1(){
+
+     var alturabajo = Math.random()*550;
+     if (alturabajo < 200){
+       alturabajo= alturabajo+200;
+     }
+      var alturaarriba = alturabajo-150;
+     console.log(alturabajo);
+     ctx = lienzo.getContext("2d");
+     ctx.fillStyle = "green";
+     ctx.fillRect(250,alturabajo, 60,600-alturabajo);
+
+     ctx.fillStyle = "green";
+     ctx.fillRect(250,0, 60,alturaarriba);
+
+
+
+     console.log("Tubo");
    }
 
    document.onkeydown = function(event) {
@@ -54,8 +79,8 @@
 
      switch (teclaC) {
        case 38: //arriba
-       var cont = 0
-
+        velocidad= 0;
+        ctx.clearRect(50, 50, posX, posY);
          posY = posY - 100;
 
          break;
